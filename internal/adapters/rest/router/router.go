ddf3ee099991e88cc777ed07f9a3f4a57ccf8096
lazylex <lazylex@mail.ru>
2023-09-09 14:15:39 +0300

    Пути изменены на более соответствующие сервису

--- internal/adapters/rest/router/router.go
+++ internal/adapters/rest/router/router.go
@@ -23,17 +23,18 @@ func New(cfg *config.Config, service *service.Service, logger *slog.Logger) *chi
 
 	router.Use(middleware.Recoverer)
 	router.Use(middleware.RequestID)
-	// TODO изменить пути на более соответствующие неймингу в сервисе
-	router.Get(apiV1+"sale/{article}", handlers.GetStockRecord)
-	router.Get(apiV1+"sale/amount/{article}", handlers.GetAmountInStock)
-	router.Put(apiV1+"sale/amount/{article}/{amount}", handlers.UpdateAmountInStock)
-	router.Put(apiV1+"sale/price/{article}/{price}", handlers.UpdatePriceInStock)
-	router.Post(apiV1+"sale/add/{article}/{amount}/{price}/{name}", handlers.AddToStock)
+
+	router.Get(apiV1+"stock/{article}", handlers.GetStockRecord)
+	router.Get(apiV1+"stock/amount/{article}", handlers.GetAmountInStock)
+	router.Put(apiV1+"stock/amount/{article}/{amount}", handlers.UpdateAmountInStock)
+	router.Put(apiV1+"stock/price/{article}/{price}", handlers.UpdatePriceInStock)
+	router.Post(apiV1+"stock/add/{article}/{amount}/{price}/{name}", handlers.AddToStock)
 
 	router.Get(apiV1+"sold/amount/{article}", handlers.GetSoldAmount)
 	router.Get(apiV1+"sold/amount/{article}/{from}/{to}", handlers.GetSoldAmountInSpan)
 
 	router.Post(apiV1+"sale/make", handlers.MakeLocalSale)
+
 	router.Post(apiV1+"reservation/make", handlers.MakeReservation)
 	router.Put(apiV1+"reservation/cancel/{order_number}", handlers.CancelReservation)
 
