7a5847ecb02caedf15af669198fb047deb1edcfc
lazylex <lazylex@mail.ru>
2023-10-31 17:06:19 +0300

    Сделано сохранение метрики для get-запросов без идентификатора

--- internal/adapters/rest/middlewares/request_metrics/request_metrics.go
+++ internal/adapters/rest/middlewares/request_metrics/request_metrics.go
@@ -4,6 +4,7 @@ import (
 	"github.com/lazylex/watch-store/store/internal/metrics"
 	"github.com/prometheus/client_golang/prometheus"
 	"net/http"
+	"strings"
 )
 
 type MiddlewareRequests struct {
@@ -15,10 +16,17 @@ func New(metrics *metrics.Metrics) *MiddlewareRequests {
 	return &MiddlewareRequests{metrics: metrics}
 }
 
-// RequestsInc увеличивает счетчик http-запросов к приложению. Добавляется метка path, содержащая путь запроса
+// RequestsInc увеличивает счетчик http-запросов к приложению. Добавляется метка path, содержащая путь запроса.
+// Метка path для GET-запросов сохраняется без идентификатора
 func (m *MiddlewareRequests) RequestsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		m.metrics.HTTP.Requests.With(prometheus.Labels{metrics.PATH: r.RequestURI}).Inc()
+		if r.Method == http.MethodGet {
+			m.metrics.HTTP.Requests.With(prometheus.Labels{
+				metrics.PATH: string([]rune(r.RequestURI)[0 : strings.LastIndex(r.RequestURI, "/")+1]),
+			}).Inc()
+		} else {
+			m.metrics.HTTP.Requests.With(prometheus.Labels{metrics.PATH: r.RequestURI}).Inc()
+		}
 		next.ServeHTTP(rw, r)
 	})
 }
