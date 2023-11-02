fa27257dabba6e6892e1b1c112967b923292092d
lazylex <lazylex@mail.ru>
2023-11-02 15:52:59 +0300

    Удалены лишние элементы

--- internal/adapters/rest/request/request.go
+++ internal/adapters/rest/request/request.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"github.com/go-chi/chi"
 	"github.com/lazylex/watch-store/store/internal/adapters/rest/response"
-	"github.com/lazylex/watch-store/store/internal/domain/aggregates/reservation"
 	"github.com/lazylex/watch-store/store/internal/domain/value_objects/article"
 	"github.com/lazylex/watch-store/store/internal/dto"
 	"github.com/lazylex/watch-store/store/internal/helpers/constants/prefixes"
@@ -15,13 +14,11 @@ import (
 )
 
 const (
-	Article     = "article"
-	Products    = "products[]"
-	Amount      = "amount"
-	OrderNumber = "order_number"
-	Status      = "status"
-	From        = "from"
-	To          = "to"
+	Article  = "article"
+	Products = "products[]"
+	Amount   = "amount"
+	From     = "from"
+	To       = "to"
 )
 
 // requestErr добавляет к тексту ошибки префикс, указывающий на её принадлежность к запросу
@@ -29,47 +26,13 @@ func requestErr(text string) error {
 	return errors.New(prefixes.RequestErrorsPrefix + text)
 }
 
-var ErrIncorrectOrder = requestErr("invalid order number passed")
 var ErrIncorrectDate = requestErr("invalid date passed")
-var ErrIncorrectReservationStatus = requestErr("invalid order status passed")
 var ErrEmptyProductsData = requestErr("product data not sent")
 var ErrEmptyFromDate = requestErr("no 'from' date in request")
 
 // GetArticleUsingChi возвращает артукул продукта
 func GetArticleUsingChi(r *http.Request) article.Article {
 	return article.Article(chi.URLParam(r, Article))
-}
-
-// GetStatusFromURLQuery возвращает статус заказа, если он есть в запросе. При отсутствии статуса в заголовок ответа
-// записывается http.StatusBadRequest, и возвращается ошибка ErrIncorrectReservationStatus, которая так же записывается
-// в лог
-func GetStatusFromURLQuery(w http.ResponseWriter, r *http.Request, logger *slog.Logger) (uint, error) {
-	parseUint, err := strconv.ParseUint(r.URL.Query().Get(Status), 10, 64)
-	if err != nil {
-		response.WriteHeaderAndLogAboutBadRequest(w, logger, ErrIncorrectReservationStatus)
-	}
-
-	return uint(parseUint), err
-}
-
-// GetOrderUsingChi возвращает номер заказа, если он есть в запросе. При отсутствии номера в заголовок ответа
-// записывается http.StatusBadRequest, и возвращается ошибка ErrIncorrectOrder, которая так же записывается в лог
-func GetOrderUsingChi(w http.ResponseWriter, r *http.Request, logger *slog.Logger) (reservation.OrderNumber, error) {
-	order, err := strconv.Atoi(chi.URLParam(r, OrderNumber))
-	if err != nil {
-		response.WriteHeaderAndLogAboutBadRequest(w, logger, ErrIncorrectOrder)
-	}
-	return reservation.OrderNumber(order), err
-}
-
-// GetOrderFromURLQuery возвращает номер заказа, если он есть в запросе. При отсутствии номера в заголовок ответа
-// записывается http.StatusBadRequest, и возвращается ошибка ErrIncorrectOrder, которая так же записывается в лог
-func GetOrderFromURLQuery(w http.ResponseWriter, r *http.Request, logger *slog.Logger) (reservation.OrderNumber, error) {
-	order, err := strconv.Atoi(r.URL.Query().Get(OrderNumber))
-	if err != nil {
-		response.WriteHeaderAndLogAboutBadRequest(w, logger, ErrIncorrectOrder)
-	}
-	return reservation.OrderNumber(order), err
 }
 
 // GetProductDTOs возращает []dto.ProductDTO, полученный из аргументов запроса, содержащего данные о продукте в таком
