35f5ee525ca460c74f3ab12e170fe190f6826a92
lazylex <lazylex@mail.ru>
2023-12-06 23:23:07 +0300

    Сделана заготовка для вывода данных о БД

--- pkg/db-viewer/db_reader/reader.go
+++ pkg/db-viewer/db_reader/reader.go
@@ -3,17 +3,61 @@ package db_reader
 import (
 	"context"
 	"database/sql"
+	"errors"
+	"io"
+	"log/slog"
+	"net/http"
 )
 
 type Reader struct {
-	db *sql.DB
+	db     *sql.DB
+	log    *slog.Logger
+	dbName string
+	tables map[string][]string
 }
 
-func New(db *sql.DB) *Reader {
-	return &Reader{db: db}
+// Start выводит список таблиц БД по адресу localhost<:port>/tables с возможностью вывода их содержимого
+func Start(db *sql.DB, log *slog.Logger, port string) {
+	var err error
+	reader := &Reader{db: db, log: log}
+	reader.dbName = reader.readDBName()
+	if reader.dbName == "" {
+		log.Error("empty db name, so no tables will viewed")
+		return
+	}
+	if reader.tables, err = reader.readTables(); err != nil {
+		log.Error("can't read tables info, so no tables will viewed")
+		return
+	}
+
+	http.HandleFunc("/tables", reader.displayTablesList)
+	err = http.ListenAndServe("port", nil)
+	if err != nil {
+		if errors.Is(err, http.ErrServerClosed) {
+			log.Error("server closed")
+		} else if err != nil {
+			log.Error("error starting server: %s", err)
+		}
+	}
+
 }
 
-func (r *Reader) ReadTables(databaseName string) (map[string][]string, error) {
+// readDBName возвращает название БД в СУБД
+func (rd *Reader) readDBName() string {
+	var name string
+	stmt := `SELECT DATABASE();`
+
+	err := rd.db.QueryRow(stmt).Scan(&name)
+	if err != nil {
+		rd.log.Error("can't read db name")
+		return ""
+	}
+
+	return name
+}
+
+// readTables возвращает названия таблиц и их столбцов в СУБД
+func (rd *Reader) readTables() (map[string][]string, error) {
 	var rows *sql.Rows
 	var err error
 	result := make(map[string][]string)
@@ -21,7 +65,7 @@ func (r *Reader) ReadTables(databaseName string) (map[string][]string, error) {
 
 	stmt := `SELECT table_name FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' AND table_schema=?;`
 
-	if rows, err = r.db.QueryContext(ctx, stmt, databaseName); err != nil {
+	if rows, err = rd.db.QueryContext(ctx, stmt, rd.dbName); err != nil {
 		return nil, err
 	}
 	for rows.Next() {
@@ -35,7 +79,7 @@ func (r *Reader) ReadTables(databaseName string) (map[string][]string, error) {
 	for table, _ := range result {
 		stmt = `SELECT column_name FROM information_schema.columns WHERE table_schema=? AND table_name=?;`
 
-		if rows, err = r.db.QueryContext(ctx, stmt, databaseName, table); err != nil {
+		if rows, err = rd.db.QueryContext(ctx, stmt, rd.dbName, table); err != nil {
 			return nil, err
 		}
 		for rows.Next() {
@@ -48,4 +92,24 @@ func (r *Reader) ReadTables(databaseName string) (map[string][]string, error) {
 	}
 
 	return result, nil
+}
+
+// tableNames возвращает список с названиями таблиц
+func (rd *Reader) tableNames() []string {
+	var result []string
+	for name, _ := range rd.tables {
+		result = append(result, name)
+	}
+	return result
+}
+
+// displayTablesList отображает страницу со списком таблиц
+func (rd *Reader) displayTablesList(w http.ResponseWriter, r *http.Request) {
+	linkStyle := makeStyles(map[string]string{"color": "black", "text-decoration": "none"})
+	for _, name := range rd.tableNames() {
+		_, err := io.WriteString(w, makeLink(name, makePath([]string{"tables", name}), linkStyle)+"<br>")
+		if err != nil {
+			return
+		}
+	}
 }
--- pkg/db-viewer/db_reader/view.go
+++ pkg/db-viewer/db_reader/view.go
@@ -1,1 +1,32 @@
 package db_reader
+
+import "fmt"
+
+// makeLink возвращает строку, содержащую html-ссылку. Параметры - текст ссылки, якорь и стили вида
+// "style1:value1;style2:value2;"
+func makeLink(text, link, styles string) string {
+	return fmt.Sprintf(`<a href="%s" style="%s">%s</a>`, link, styles, text)
+}
+
+// maleStyles возвращает строку стилей вида "style1:value1;style2:value2;" полученные из мапы вида "map[стиль]значение"
+func makeStyles(styles map[string]string) string {
+	var result string
+
+	for style, value := range styles {
+		result += fmt.Sprintf("%s:%s;", style, value)
+	}
+
+	return result
+}
+
+// makePath объединяет переданные строки в путь вида "строка1/строка2/.../строкаN"
+func makePath(pathParts []string) string {
+	var result string
+	for idx, part := range pathParts {
+		result += part
+		if idx != len(pathParts) {
+			result += "/"
+		}
+	}
+	return result
+}
