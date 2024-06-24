eed571d24a50785faf0a91d5c014e9f409a44eff
lazylex <lazylex@mail.ru>
2024-06-24 22:29:52 +0300

    Удалены не используемые структуры

--- internal/domain/aggregates/reservation/reservation.go
+++ internal/domain/aggregates/reservation/reservation.go
@@ -1,10 +1,5 @@
 package reservation
 
-import (
-	"github.com/lazylex/watch-store-store/internal/domain/value_objects/product"
-	"time"
-)
-
 type OrderNumber int
 
 const MaxCashRegisterNumber = 10
@@ -16,10 +11,3 @@ const (
 	Finished
 	Cancel
 )
-
-type Reservation struct {
-	product     []*product.Product
-	date        time.Time
-	orderNumber OrderNumber
-	state       uint
-}
